b622ad038ca032169c33cee4283ad1de963b361f
Zinedeen DAVOUSSE <zdavousse@gmail.com>
2021-03-30 20:07:17 +0200

    using react component & make a loading page while the contents loading

--- front/src/Form.js
+++ front/src/Form.js
@@ -5,57 +5,80 @@ import Grid from '@material-ui/core/Grid';
 import Tile from "./components/Tile/Tile";
 import {Button} from "@material-ui/core";
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    paper: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        color: theme.palette.text.secondary,
+class Form extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            configs: [],
+            classes: makeStyles((theme) => ({
+                root: {
+                    flexGrow: 1,
+                },
+                paper: {
+                    padding: theme.spacing(2),
+                    textAlign: 'center',
+                    color: theme.palette.text.secondary,
+                }
+            })),
+        }
     }
-}));
 
-const Form = () => {
-    const classes = useStyles();
+    componentDidMount() {
+        let header = new Headers();
+        let init = {
+            method: 'get',
+            header: header,
+            mode: 'cors',
+            cache: 'default',
+        };
 
-    let header = new Headers();
-    let init = {
-        method: 'get',
-        header: header,
-        mode: 'cors',
-        cache: 'default',
-    };
-
-    const configs = fetch("https://api.pfa.dietz.dev/configs", init)
-        .then(
+        fetch("https://api.pfa.dietz.dev/configs", init).then(result => result.json()).then(
             (result) => {
-                console.log(result.json());
-                return result.json();
+                this.setState(
+                    {
+                        configs: result.items,
+                        isLoaded: true,
+                    }
+                )
             },
             (error) => {
                 console.error(error);
-                return {};
+                this.setState(
+                    {
+                        configs: {},
+                        isLoaded: true,
+                    }
+                )
             }
         )
+    }
 
-    return (
-        <Grid container className={classes.root} spacing={2} justify={"center"}>
-            <Grid item xs={11}>
-                <form>
-                    <Grid container justify="center" spacing={2}>
-                        {Object.keys(configs).map((index) => (
-                            <Grid key={configs[index].title} xs={4} item>
-                                {(console.log(configs))}
-                                <Tile tile={configs[index]} key={configs[index].title}/>
-                            </Grid>
-                        ))}
-                    </Grid>
-                    <Button type={"submit"} variant={"outlined"}>Générer</Button>
-                </form>
+    render() {
+        const {isLoaded, configs, classes} = this.state;
+
+        if (!isLoaded){
+            return (<div>Loading...</div>)
+        }
+        return (
+            <Grid container className={classes.root} spacing={2} justify={"center"}>
+                {(console.log(configs))}
+                <Grid item xs={11}>
+                    <form>
+                        <Grid container justify="center" spacing={2}>
+                            {Object.keys(configs).map((index) => (
+                                <Grid key={configs[index].title} xs={4} item>
+                                    {(console.log(configs))}
+                                    <Tile tile={configs[index]} key={configs[index].title}/>
+                                </Grid>
+                            ))}
+                        </Grid>
+                        <Button type={"submit"} variant={"outlined"}>Générer</Button>
+                    </form>
+                </Grid>
             </Grid>
-        </Grid>
-    );
-};
+        );
+    }
+}
 
 export default Form;
