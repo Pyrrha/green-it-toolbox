d398510a5107712f3684fe88d438c7b9818ffeee
Zinedeen DAVOUSSE <zdavousse@gmail.com>
2021-03-30 19:32:00 +0200

    using api configs

--- front/src/Form.js
+++ front/src/Form.js
@@ -18,43 +18,40 @@ const useStyles = makeStyles((theme) => ({
 
 const Form = () => {
     const classes = useStyles();
-    const forms = [
-        {
-            title: "Language",
-            choices: {
-                "py" : "Python",
-                "js" : "JavaScript",
-                "php" : "Php",
+
+    let header = new Headers();
+    let init = {
+        method: 'get',
+        header: header,
+        mode: 'cors',
+        cache: 'default',
+    };
+
+    const configs = fetch("https://api.pfa.dietz.dev/configs", init)
+        .then(
+            (result) => {
+                console.log(result.json());
+                return result.json();
             },
-            text: ""
-        },
-        {
-            title: "Tool used",
-            options: {
-                "git" : "Git",
-                "github" : "Github Actions",
-                "docker" : "Docker",
-            },
-            text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        },
-        {
-            title: "Mode",
-            text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        }
-    ];
+            (error) => {
+                console.error(error);
+                return {};
+            }
+        )
 
     return (
         <Grid container className={classes.root} spacing={2} justify={"center"}>
             <Grid item xs={11}>
                 <form>
                     <Grid container justify="center" spacing={2}>
-                        {forms.map((value) => (
-                            <Grid key={value.title} xs={4} item>
-                                <Tile tile={value} key={value.title}/>
+                        {Object.keys(configs).map((index) => (
+                            <Grid key={configs[index].title} xs={4} item>
+                                {(console.log(configs))}
+                                <Tile tile={configs[index]} key={configs[index].title}/>
                             </Grid>
                         ))}
                     </Grid>
-                    <Button type={"submit"} variant={"outlined"}>Submit</Button>
+                    <Button type={"submit"} variant={"outlined"}>Générer</Button>
                 </form>
             </Grid>
         </Grid>
