b8fd9784bb94f44810f7f4dbc0cf542aa6d2bf22
Tristan Dietz <tristan.dietz@laposte.net>
2021-04-01 16:56:10 +0200

    Adding dynamic dispatch

--- back/viewer/visitor.py
+++ back/viewer/visitor.py
@@ -1,8 +1,36 @@
 from abc import ABC, abstractmethod
 
-from ast.node import Node
+from abstraction.group import Group
+from abstraction.job import Job
+from abstraction.node import Node
+from abstraction.pipeline import Pipeline
+from abstraction.step import Step
 
 class Visitor(ABC):
+    def visit(self, node: Node):
+        if (isinstance(node, Group)):
+            self.__visitGroup(node)
+        elif (isinstance(node, Job)):
+            self.__visitJob(node)
+        elif (isinstance(node, Pipeline)):
+            self.__visitPipeline(node)
+        elif (isinstance(node, Step)):
+            self.__visitStep(node)
+        # TODO else case
+            
+
     @abstractmethod
-    def visit(node: Node):
+    def __visitGroup(group):
+        pass
+
+    @abstractmethod
+    def __visitJob(job):
+        pass
+
+    @abstractmethod
+    def __visitPipeline(pipeline):
+        pass
+
+    @abstractmethod
+    def __visitStep(step):
         pass
